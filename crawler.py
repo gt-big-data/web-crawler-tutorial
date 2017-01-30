6ff742ba516958e524ed3be876a54c5d0b8351d6
Sam Marder <sam@marder.co>
2017-01-30 14:14:59 -0500

    Added all pages as HTML files.

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as Soup
 
 EXAMPLE_URL = 'http://example.com/'
-GTPD_WILDCARD_URL = 'www.police.gatech.edu/crimeinfo/crimelogs/noncrimelog.php?offset=%d'
+GTPD_WILDCARD_URL = 'http://www.police.gatech.edu/crimeinfo/crimelogs/noncrimelog.php?offset=%d'
 GTPD_URL = GTPD_WILDCARD_URL % 0
 MAIN_GTPD_URL = 'http://www.police.gatech.edu/crimeinfo/crimelogs/noncrimelog.html'
 GOOD_URL = 'http://httpstat.us/200'
@@ -15,8 +15,22 @@ def get_page(url):
         return ''
     return res.text
 
+def save_page(url, filename):
+    html = get_page(url)
+    with open('pages/%s' % filename, 'w') as f:
+        f.write(html)
+
+def open_page(filename):
+    with open('pages/%s' % filename) as f:
+        html = f.read()
+    return Soup(html, 'html.parser')
+
 print('Congrats! Everything is set up. Type `exit()` to close python.')
 
 # print(get_page(EXAMPLE_URL))
 # print(get_page(GOOD_URL))
 # print(get_page(BAD_URL))
+
+# save_page(EXAMPLE_URL)
+# save_page(GOOD_URL)
+# save_page(BAD_URL)
